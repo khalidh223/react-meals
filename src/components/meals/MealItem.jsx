64466cac65de27f039437130912b2140044fa34e
Khalid Hussain <khalid.hussain@foodsby.com>
2022-12-04 20:43:25 -0600

    Adds unconnected meals card and the components needed for its items

---
+++ src/components/meals/MealItem.jsx
@@ -0,0 +1,46 @@
+import { Add } from "@mui/icons-material";
+import {
+  Button,
+  ListItem,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import AddRemoveMealButton from "./AddRemoveMealButton";
+
+
+const MealItem = ({ meal }) => {
+  return (
+    <>
+      <ListItem sx={{ paddingBottom: "0" }}>
+        <ListItemText
+          sx={{ marginBottom: "0" }}
+          primary={
+            <Typography variant="body6" fontWeight={"bold"}>
+              {meal.name}
+            </Typography>
+          }
+          secondary={meal.description}
+        />
+        <AddRemoveMealButton/>
+      </ListItem>
+      <ListItem sx={{ paddingTop: "0", marginTop: "0" }}>
+        <ListItemText>
+          <Typography variant="h6" sx={{ color: "red", fontWeight: "900" }}>
+            {meal.price}
+          </Typography>
+        </ListItemText>
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: "darkred" }}
+          color="error"
+          startIcon={<Add />}
+        >
+          Add
+        </Button>
+      </ListItem>
+    </>
+  );
+};
+
+export default MealItem;
